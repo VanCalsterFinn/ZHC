61971f3a7b1211ad8b5ec69d811238766dffb1d8
VanCalsterFinn <95765255+VanCalsterFinn@users.noreply.github.com>
2025-12-15 21:59:33 +0100

    current HOME dev

--- controller/serializers.py
+++ controller/serializers.py
@@ -17,6 +17,7 @@ class ZoneSerializer(serializers.ModelSerializer):
         fields = [
             'id',
             'name',
+            'pin',
             'current_temperature',
             'target_temperature',
             'manual_override'
